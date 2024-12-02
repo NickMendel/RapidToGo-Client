cc0d35e495598a5d39bffd86a3bfef166518e0b4
NickMendel <nick.mendel@web.de>
2024-12-02 17:12:43 +0100

    init welcome page

---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class='pt-5 flex flex-column align-items-center'>
+        <div class='mb-4'>
+            <h1>{{ title }}</h1>
+        </div>
+        <div class=''>
+            <ButtonGroup class=''>
+                <PrimeButton class='mx-3' type='button' label='Anmelden' icon='pi pi-user' severity='primary'
+                    size='large' variant='text' raised />
+                <PrimeButton type='button' label='Neu hier? Registrieren!' icon='pi pi-user-plus' severity='secondary'
+                    size='large' variant='text' raised />
+            </ButtonGroup>
+        </div>
+    </div>
+
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+const title = ref<string>('Willkommen bei RapidToGo!');
+
+</script>
+
+<style scoped></style>
